e246763e75eb8735bd5e1e620c8150cc94d30152
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-22 03:06:01 +0200

    Implemented on focus.

--- src/main/java/spec/Main2.java
+++ src/main/java/spec/Main2.java
@@ -3,7 +3,12 @@ package spec;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import java.io.File;
+
+import static java.awt.Event.GOT_FOCUS;
+import static java.awt.Event.LOST_FOCUS;
 
 public class Main2 extends JPanel {
 
@@ -22,9 +27,33 @@ public class Main2 extends JPanel {
         frame.setFocusable(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addWindowFocusListener(focusListener());
 
         app = new Application(this, new File("src/main/resources").toURI().toURL());
+        onFocus(GOT_FOCUS);
         app.start();
+    }
+
+    private WindowFocusListener focusListener() {
+        return new WindowFocusListener() {
+            @Override
+            public void windowGainedFocus(WindowEvent event) {
+                if (app != null) {
+                    onFocus(GOT_FOCUS);
+                }
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent event) {
+                if (app != null) {
+                    onFocus(LOST_FOCUS);
+                }
+            }
+        };
+    }
+
+    private void onFocus(int status) {
+        app.handleEvent(new Event(null, status, null));
     }
 
     @Override
