8742a3f308a62f973e207bc3702870e82780ad05
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-28 12:58:10 +0200

    Create hello world asm.

---
+++ src/main/resources/test/hello_world.asm
@@ -0,0 +1,38 @@
+        .project hello_worls.rks
+        CPU     8080
+        .ORG    0000h
+        DB      04h, 00h    ; RKS HEADER/START MEMORY 
+        DB      30h, 00h    ; RKS HEADER/END MEMORY
+        
+        LXI     H, STRING
+        CALL    MSG
+        JMP     0004h
+
+;
+STRING: DB      "HELLO WORLD", 0dh, 0ah, '$'
+
+;
+BDOS    EQU     0C037H  ;LIK PRINT CHAR PROCEDURE
+
+;
+;MESSAGE OUTPUT ROUTINE
+;
+MSG:    MOV     A,M     ; Get data
+        CPI     '$'     ; End?
+        RZ
+        CALL    PCHAR   ; Output
+        INX     H       ; Next
+        JMP     MSG     ; Do all
+;
+;
+;CHARACTER OUTPUT ROUTINE
+;
+PCHAR:  PUSH    PSW
+        PUSH    D
+        PUSH    H
+        MOV     C,M
+        CALL    BDOS
+        POP     H
+        POP     D
+        POP     PSW
+        RET
