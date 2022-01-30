7e2941172e56e18bb171cad33fd2926cfe401afb
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-30 16:52:44 +0200

    Replace 0ABH -> 0ABh.

--- src/main/resources/test/hello_world.asm
+++ src/main/resources/test/hello_world.asm
@@ -1,28 +1,28 @@
         .project hello_world.rks
         CPU     8080
-        .ORG    0000h
-        DB      04h, 00h    ; RKS HEADER/START MEMORY 
-        DB      32h, 00h    ; RKS HEADER/END MEMORY
+        .ORG    00000h
+        DB      004h, 000h    ; RKS HEADER/START MEMORY
+        DB      032h, 000h    ; RKS HEADER/END MEMORY
         
         LXI     H, string
         CALL    msg
-        JMP     0C800h      ; LIK MONITOR-1M
+        JMP     0C800h        ; LIK MONITOR-1M
 
 ;
-string: DB      0dh, 0ah, "HELLO WORLD", 0dh, 0ah, '$'
+string: DB      00Dh, 00Ah, "HELLO WORLD", 00Dh, 00Ah, '$'
 
 ;
-bdos    EQU     0C037H  ;LIK PRINT CHAR PROCEDURE
+bdos    EQU     0C037h        ; LIK PRINT CHAR PROCEDURE
 
 ;
 ;MESSAGE OUTPUT ROUTINE
 ;
-msg:    MOV     A,M     ; Get data
-        CPI     '$'     ; End?
+msg:    MOV     A,M           ; Get data
+        CPI     '$'           ; End?
         RZ
-        CALL    pchar   ; Output
-        INX     H       ; Next
-        JMP     msg     ; Do all
+        CALL    pchar         ; Output
+        INX     H             ; Next
+        JMP     msg           ; Do all
 ;
 ;
 ;CHARACTER OUTPUT ROUTINE
