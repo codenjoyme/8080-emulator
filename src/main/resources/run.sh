caad228ee324f63cbe2a86959e3c2e3856761da3
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-02-02 01:58:07 +0200

    Fixed run script.

---
+++ src/main/resources/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+BLUE=94
+GRAY=89
+YELLOW=93
+
+color() {
+    message=$1
+    [[ "$2" == "" ]] && color=$YELLOW || color=$2
+    echo "[${color}m${message}[0m"
+}
+
+eval_echo() {
+    command=$1
+    [[ "$2" == "" ]] && color=$BLUE || color=$2
+    color "${command}" $color
+    echo
+    eval $command
+}
+
+eval_echo "ROOT=$(pwd)"
+eval_echo "PARAM=$1"
+eval_echo "java -jar $ROOT/emulator-1.0.jar $PARAM"
+
+echo
+color "Press Enter to continue"
+read
