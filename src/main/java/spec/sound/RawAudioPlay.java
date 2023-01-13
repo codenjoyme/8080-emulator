1be1fd2226b56662f5b45edf4cec1a95e2289f9e
Oleksandr Baglai <oleksandr_baglai@epam.com>
2023-01-13 13:45:36 +0300

    Output random wave.

--- src/main/java/spec/sound/RawAudioPlay.java
+++ src/main/java/spec/sound/RawAudioPlay.java
@@ -3,6 +3,7 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
+import java.util.Random;
 
 // Thanks to the https://stackoverflow.com/a/32891220
 public class RawAudioPlay {
@@ -10,30 +11,19 @@ public class RawAudioPlay {
     public static void main(String[] args) {
         try {
             // select audio format parameters
-            AudioFormat af = new AudioFormat(24000, 16, 1, true, false);
+            AudioFormat af = new AudioFormat(2000, 8, 1, false, false);
             DataLine.Info info = new DataLine.Info(SourceDataLine.class, af);
             SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
 
             // generate some PCM data (a sine wave for simplicity)
-            byte[] buffer = new byte[64];
-            double step = Math.PI / buffer.length;
-            double angle = Math.PI * 2;
-            int i = buffer.length;
-            while (i > 0) {
-                double sine = Math.sin(angle);
-                int sample = (int) Math.round(sine * 32767);
-                buffer[--i] = (byte) (sample >> 8);
-                buffer[--i] = (byte) sample;
-                angle -= step;
-            }
+            byte[] buffer = new byte[10000];
+            new Random().nextBytes(buffer);
 
             // prepare audio output
-            line.open(af, 4096);
+            line.open(af, buffer.length);
             line.start();
             // output wave form repeatedly
-            for (int n=0; n<500; ++n) {
-                line.write(buffer, 0, buffer.length);
-            }
+            line.write(buffer, 0, buffer.length);
             // shut down audio
             line.drain();
             line.stop();
