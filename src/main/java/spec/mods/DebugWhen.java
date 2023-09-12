9dc4b579adf4de4ae49867ff49e6883da84dd347
Oleksandr Baglai <oleksandr_baglai@epam.com>
2023-09-12 16:10:52 +0300

    Added ability to change cpu when debug.

--- src/main/java/spec/mods/DebugWhen.java
+++ src/main/java/spec/mods/DebugWhen.java
@@ -1,8 +1,16 @@
 package spec.mods;
 
+import spec.Cpu;
+
+import java.util.function.Consumer;
+
 public class DebugWhen extends WhenPC {
 
     public DebugWhen(int addr, Runnable trigger) {
         super(addr, cpu -> trigger.run());
+    }
+
+    public DebugWhen(int addr, Consumer<Cpu> trigger) {
+        super(addr, trigger::accept);
     }
 }
