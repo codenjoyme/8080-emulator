f926159a461f2a330df5c445a22da15f77bdf55b
Oleksandr Baglai <oleksandr_baglai@epam.com>
2023-09-12 16:31:18 +0300

    Fixed CMP commands.

--- src/main/java/spec/assembler/Parity.java
+++ src/main/java/spec/assembler/Parity.java
@@ -3,6 +3,10 @@ package spec.assembler;
 public class Parity {
 
     public static final boolean[] parity = new boolean[256];
+    public static final boolean[] half_carry_table =
+            { false, false, true, false, true, false, true, true };
+    public static final boolean[] sub_half_carry_table =
+            { false, true, true, true, false, false, false, true };
 
     static {
         for (int i = 0; i < 256; i++) {
--- src/main/java/spec/assembler/command/math/flag/CMP_R.java
+++ src/main/java/spec/assembler/command/math/flag/CMP_R.java
@@ -6,12 +6,8 @@ import spec.assembler.Command;
 
 import java.util.List;
 
-import static spec.Constants.x0F;
-import static spec.Constants.x100;
-import static spec.Registry.T4h;
-import static spec.Registry.T7s;
-import static spec.WordMath.lo;
 import static spec.assembler.Parity.parity;
+import static spec.assembler.Parity.sub_half_carry_table;
 
 // TODO test me
 public class CMP_R extends Command {
@@ -41,13 +37,14 @@ public class CMP_R extends Command {
     }
 
     public static void cmp8(Registry r, int a, int b) {
-        int wans = a - b;
-        int ans = lo(wans);
-
-        r.ts((ans & T7s) != 0);
-        r.tz(ans == 0);
-        r.tc((wans & x100) != 0);
-        r.th((((a & x0F) - (b & x0F)) & T4h) != 0);
-        r.tp(parity[ans]);
+        int work16 = a - b;
+        int index = ((a & 0x88) >> 1) |
+                    ((b & 0x88) >> 2) |
+                    ((work16 & 0x88) >> 3);
+        r.ts((work16 & 0x80) != 0);
+        r.tz((work16 & 0xff) == 0);
+        r.th(!sub_half_carry_table[index & 0x7]);
+        r.tc((work16 & 0x0100) != 0);
+        r.tp(parity[work16 & 0xff]);
     }
 }
