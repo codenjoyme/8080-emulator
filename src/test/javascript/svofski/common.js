29bc6bdc26ce53b00ba78b88c684893e3f8c2030
Oleksandr Baglai <Oleksandr_Baglai@epam.com>
2024-12-05 23:43:51 +0300

    Ported to Java. Load program from file.

--- src/test/javascript/svofski/common.js
+++ src/test/javascript/svofski/common.js
@@ -8,6 +8,16 @@ function replacer(key, value) {
   return value;
 }
 
+export function readFile(fileName) {
+  const filePath = path.join(__dirname, '', fileName);
+  try {
+    return fs.readFileSync(filePath, "utf-8");
+  } catch (error) {
+    console.error(`Failed to read the file: ${error}`);
+    return "";
+  }
+}
+
 export function assertCall(fileName, value, silent = true) {
   const testTitle = expect.getState().currentTestName || "unknown";
   const filePath = path.join(__dirname, 'actual', fileName);
@@ -37,4 +47,57 @@ export function assertCall(fileName, value, silent = true) {
     expect(actual).toEqual(expected);
   }
   return value;
+}
+
+export let recorder = (() => {
+  var data = [];
+  var method = null;
+  let collect = (input) => {
+    let found = data.find((item) => {
+      return JSON.stringify(item) === JSON.stringify(input);
+    });
+    if (!found) {
+      data.push(input);
+    }
+  };
+  let decorate = (object, methodName, fields) => {
+    method = methodName;
+    let old = object[method];
+    object[method] = (...args) => {
+      let fieldsData = !fields ? null : fields.map(it => ({
+        name: it,
+        value: JSON.parse(JSON.stringify(object[it]))
+      }));
+
+      let result = old.apply(object, args);
+
+      collect({
+        input: args,
+        fields: fieldsData,
+        result: typeof result == 'undefined' ? null : result,
+      });
+      return result;
+    }
+  };
+  let result = () => {
+    return JSON.stringify(data, null, 2);
+  };
+  return {
+    decorate,
+    collect,
+    result,
+    method: () => method,
+  };
+});
+
+let recs = [];
+export let record = function (object, methodName, fields) {
+  let rec = recorder();
+  rec.decorate(object, methodName, fields);
+  recs.push(rec);
+}
+export let assertAllRecords = function(dir) {
+  recs.forEach((rec) => {
+    assertCall(dir + 'method/' + rec.method() + '.json', rec.result());
+  });
 }
